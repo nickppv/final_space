92ebbc2219f76728818a6859dc33f89e3fa340c2
Николай Попов <nickpv@yandex.ru>
2023-07-10 03:03:37 +0700

    add application "space_agency"

--- nasa_project/nasa_project/settings.py
+++ nasa_project/nasa_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'space_agency.apps.SpaceAgencyConfig'
 ]
 
 MIDDLEWARE = [
---
+++ nasa_project/space_agency/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpaceAgencyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'space_agency'
