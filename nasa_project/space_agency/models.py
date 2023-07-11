b3e96ede5ab6cee8f351dd0e07fb9ec4990f044e
Николай Попов <nickpv@yandex.ru>
2023-07-12 03:35:57 +0700

    add table for images

--- nasa_project/space_agency/models.py
+++ nasa_project/space_agency/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Galery(models.Model):
+    name = models.CharField(
+        max_length=50,
+        verbose_name='Название изображения'
+        )
+    image = models.ImageField(upload_to='images')
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание'
+        )
+
+    class Meta:
+        verbose_name = "Изображение"
+        verbose_name_plural = "s"
+
+    def __str__(self):
+        return self.name
