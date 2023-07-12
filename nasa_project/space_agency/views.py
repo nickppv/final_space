08420881a64640316fa33f3fdb2fe2e35d735722
Николай Попов <nickpv@yandex.ru>
2023-07-13 06:56:35 +0700

    slider is almost ready

--- nasa_project/space_agency/views.py
+++ nasa_project/space_agency/views.py
@@ -4,5 +4,9 @@ from .models import Galery
 
 def index(request):
     all_pictures = Galery.objects.all()
-    context = {'all_pictures': all_pictures}
+    last_picture = all_pictures.last()
+    context = {
+        'all_pictures': all_pictures,
+        'last_picture': last_picture
+        }
     return render(request, 'space_agency/index.html', context)
