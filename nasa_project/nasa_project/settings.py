64f99355bffaac493681a2ab19fb954ec0a83b7c
Николай Попов <nickpv@yandex.ru>
2023-07-19 03:40:30 +0700

    убрал secret_key в скрытый файл

--- nasa_project/nasa_project/settings.py
+++ nasa_project/nasa_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
-from .key_for_db import PASS
+from .key_for_db import PASS, SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cbp=sx0u)o0!4*$+-^-t1izm516y(3kjxc2#9)x=(^!hsd+y9s'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
